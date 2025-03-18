0d693412e9041d0f76554b8d80fb74f3f99a5962
Yuval Shamir <yuvalsh18@gmail.com>
2025-03-18 21:25:58 +0200

    created Header and Home components created routing in App component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,16 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import '@mui/material'
 import { Typography } from '@mui/material'
-
+import Header from './Header'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <header>
-        <Typography variant="h1">Welcome to ono cafeteria!</Typography>
-      </header>
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        {/* <Route path="/help" element={<Help />} /> */}
+      </Routes>
 
       <Typography variant='p'>
         This is the first page of our cafeteria. We are using React with Vite.
