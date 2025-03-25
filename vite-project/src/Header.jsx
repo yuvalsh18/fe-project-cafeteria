dcaa26f5f07ee7e92e10c5700bcfc4cd6a217031
Yuval Shamir <yuvalsh18@gmail.com>
2025-03-25 20:22:31 +0200

    added buttons and navigation hook on header component. styling of the buttons and site title.

--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -1,16 +1,36 @@
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+    const navigate = useNavigate();
+
     return (
       <AppBar>
         <Toolbar>
-          <Typography variant="h6" component="div">
-            Grade Insight
+          <Typography variant="h5" component="div" onClick={() => navigate('/')}>
+            Ono Cafetefia
           </Typography>
+          <Button 
+            color="inherit" 
+            onClick={() => navigate('/info')} 
+            sx={{ '&:focus': { outline: 'none' } }}
+          >
+            Info
+          </Button>
+          <Box sx={{ mx: 1 }}>|</Box>
+          <Button 
+            color="inherit" 
+            onClick={() => navigate('/help')} 
+            sx={{ '&:focus': { outline: 'none' } }}
+          >
+            Help
+          </Button>
+          <Box sx={{ flexGrow: 1 }} />
         </Toolbar>
       </AppBar>
     );
    }
-  
